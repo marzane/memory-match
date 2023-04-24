f459a292bbd8412606a57c20dd92d15ec7e3c84a
marz <marzaneporquesi@gmail.com>
2023-04-24 11:40:14 +0200

    añadido segundo metodo generar cartas

--- juego.js
+++ juego.js
@@ -1,5 +1,5 @@
-const CARTAS_POR_FILA = 4;          // numero de cartas que hay en cada fila de cartas
-const CARTAS_NUM_FILAS = 2;         // numero de filas
+const CARTAS_POR_FILA = 6;          // numero de cartas que hay en cada fila de cartas
+const CARTAS_NUM_FILAS = 3;         // numero de filas
 const CARTAS_REPETIDAS = 2;         // numero de veces que se repite cada carta
 
 const CARTAS_TOTALES = CARTAS_NUM_FILAS * CARTAS_POR_FILA;
@@ -9,12 +9,12 @@ const CARTAS_EXT = ".png";
 
 window.addEventListener("DOMContentLoaded", main);
 
-function main(){
+function main() {
 
     const contCartas = document.getElementById("contenedorCartas");
-    if(contCartas){
-
-        // generar filas con cartas repetidas
+    if (contCartas) {
+        
+        // generar los numeros de las cartas
         let numeros = [];
         for(i = 0; i < CARTAS_REPETIDAS; i++){
             numerosGenerados = generarNumeros(1, CARTAS_TOTALES / CARTAS_REPETIDAS);
@@ -22,8 +22,11 @@ function main(){
         }
         
         numeros = desordenarArray(numeros);
-        console.log(numeros)
-
+        console.log(numeros);
+        
+        
+        // generar filas con cartas repetidas
+        // **********************************
         let contadorNumero = 0;
         
         for(let i = 0; i < CARTAS_NUM_FILAS; i++){
@@ -49,29 +52,59 @@ function main(){
 
             contCartas.appendChild(fila);
         }
+        
 
-
-        // generar filas sin cartas repetidas
         /*
-        for(let i = 0; i < CARTAS_NUM_FILAS; i++){
-
-            let numeros = aleatoriosSinRepetidos(CARTAS_POR_FILA, 1, CARTAS_POR_FILA);
+        // generar filas sin cartas repetidas
+        // **********************************
+        for (let i = 0; i < CARTAS_NUM_FILAS; i++) {
             let fila = crearDivImagenes(CARTAS_POR_FILA);
-            
             fila.classList.add("filaCarta");
             let imagenes = fila.childNodes;
-            if(imagenes.length > 0 && numeros.length >= imagenes.length){
-                for(let i = 0; i < imagenes.length; i++){
-                    imagenes[i].classList.add("carta");
-                    imagenes[i].src = CARTAS_RUTA + CARTAS_NOMBRE + numeros[i] + CARTAS_EXT;
-                    imagenes[i].setAttribute("data-index", numeros[i]);
+
+            if (imagenes.length > 0 && numeros.length >= imagenes.length) {
+                let auxiliar = [];
+                for (imagen of imagenes) {
+
+                    let posicion = 0;
+                    let numero = -1;
+                    let salida = true;
+                    do{
+                        numero = numeros[posicion];   // bucle para seleccionar el numero de la carta
+                        
+                        if(numero){
+                            salida = !auxiliar.includes(numero);
+                            if(salida){
+                                auxiliar.push(numero);
+                                numeros.splice(posicion, 1);
+                            } else {
+                                numero = numeros[posicion];
+                            }
+
+                        }
+                        posicion++;
+
+                    }while(numero && !salida && numeros.length > 0);
+
+                    console.log(numeros + "; auxiliar: " + auxiliar);
+
+                    if(numero && numero != -1){
+                        imagen.classList.add("carta");
+                        imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + numero + CARTAS_EXT;
+                        //imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + 0 + CARTAS_EXT;  // ocultar las cartas
+                        imagen.setAttribute("data-index", numero);
+                    } else {
+                        fila.remove(imagen);
+                    }
+
                 }
+
             }
 
-
             contCartas.appendChild(fila);
-        }*/
-        
+        }
+        */
+
     }
 
 }
@@ -79,12 +112,12 @@ function main(){
 
 
 
-function crearDivImagenes(numImagenes = 0){
+function crearDivImagenes(numImagenes = 0) {
     let contenedor = document.createElement("div");
 
-    if(typeof numImagenes == "number" && numImagenes >= 0){
+    if (typeof numImagenes == "number" && numImagenes >= 0) {
 
-        for(let i = 0; i < numImagenes; i++){
+        for (let i = 0; i < numImagenes; i++) {
             let imagen = document.createElement("img");
             contenedor.appendChild(imagen);
         }
@@ -97,97 +130,70 @@ function crearDivImagenes(numImagenes = 0){
 
 
 
-// numeros aleatorios sin repetidos
-function aleatoriosSinRepetidos(cantidad=0, min=0, max=0){
+function generarNumeros(min = 0, max = 0) {
 
     let numeros = [];
 
-    if(min < max && cantidad <= max){
-        while(numeros.length < cantidad){
+    if (typeof min == "number" && typeof max == "number" && min < max) {
 
-            let numero = Math.floor(Math.random() * (max - min + 1)) + min;
-            
-            if(!numeros.includes(numero)) {
-                numeros.push(numero);
-            }
-    
-        }
-    }
-    
-    return numeros;
-}
-
-
-
-
-function generarNumeros(min = 0, max = 0){
-
-    let numeros = [];
-
-    if(typeof min == "number" && typeof max == "number" && min < max){
-
-        for(let i = min; i <= max; i++){
+        for (let i = min; i <= max; i++) {
             numeros.push(i);
         }
-
     }
-
 
     return numeros;
 }
 
 
 
-function generarNumerosDesordenados(min = 0, max = 0){
+
+function generarNumerosDesordenados(min = 0, max = 0) {
 
     let numeros = [];
     let posiciones = [];
     let desordenados = [];
 
-    if(typeof min == "number" && typeof max == "number" && min < max){
+    if (typeof min == "number" && typeof max == "number" && min < max) {
 
-        for(let i = min; i <= max; i++){
+        for (let i = min; i <= max; i++) {
             numeros.push(i);
         }
 
-    
         max = max - 1;
         min = 0;
-        do{
+        do {
             let posicion = Math.floor(Math.random() * (max - min + 1));
-            if(!posiciones.includes(posicion)){
+            if (!posiciones.includes(posicion)) {
                 desordenados.push(numeros[posicion]);
                 posiciones.push(posicion);
             }
-        }while(desordenados.length < numeros.length);
+        } while (desordenados.length < numeros.length);
     }
-
 
     return desordenados;
 }
 
 
 
-function desordenarArray(array = []){
+
+function desordenarArray(array = []) {
 
     let posiciones = [];
     let desordenados = [];
 
-    if(Array.isArray(array) && array.length > 1){
+    if (Array.isArray(array) && array.length > 1) {
 
-    max = array.length - 1;
-    min = 0;
+        max = array.length - 1;
+        min = 0;
 
-    do {
-        let posicion = Math.floor(Math.random() * (max - min + 1));
-             if(!posiciones.includes(posicion)){
-                  desordenados.push(array[posicion]);
-                  posiciones.push(posicion);
-             }
-        }while(desordenados.length < array.length);
+        do {
+            let posicion = Math.floor(Math.random() * (max - min + 1));
+            if (!posiciones.includes(posicion)) {
+                desordenados.push(array[posicion]);
+                posiciones.push(posicion);
+            }
+        } while (desordenados.length < array.length);
     }
 
-
     return desordenados;
-
 }
