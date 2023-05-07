da77eae8c259dfb7018e80712059452e20667688
marz <marzaneporquesi@gmail.com>
2023-05-07 23:24:39 +0200

    animaciones y mas

--- juego.js
+++ juego.js
@@ -5,7 +5,6 @@ const CARTAS_REPETIDAS = 2;         // numero de veces que se repite cada carta
 const CARTAS_TOTALES = CARTAS_NUM_FILAS * CARTAS_POR_FILA;
 const CARTAS_RUTA = "imagenes/cartas/";
 const CARTAS_NOMBRE = "carta";
-const CARTAS_INVISIBLE = "Invisible";
 const CARTAS_EXT = ".png";
 
 const CLASE_CARTA_MOSTRADA = "mostrada";
@@ -25,28 +24,26 @@ function main() {
         }
 
         numeros = desordenarArray(numeros);
-        console.log(numeros);
-
 
         // generar filas con cartas repetidas
         // **********************************
         let contadorNumero = 0;
 
         for (let i = 0; i < CARTAS_NUM_FILAS; i++) {
-            let fila = crearDivImagenes(CARTAS_POR_FILA);
+            let fila = crearFilasDiv(CARTAS_POR_FILA);
             fila.classList.add("filaCarta");
-            let imagenes = fila.childNodes;
+            let cartas = fila.childNodes;
 
-            if (imagenes.length > 0 && numeros.length >= imagenes.length) {
-                for (imagen of imagenes) {
+            if (cartas.length > 0 && numeros.length >= cartas.length) {
+                for (carta of cartas) {
                     if (numeros[contadorNumero]) {
-                        imagen.classList.add("carta");
-                        imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + 0 + CARTAS_EXT;  // ocultar las cartas
-                        imagen.setAttribute("data-index", numeros[contadorNumero]);
-                        imagen.addEventListener("click", voltearCartaListener);
+                        carta.classList.add("carta");
+                        carta.setAttribute("data-index", numeros[contadorNumero]);
+                        carta.style.backgroundImage = `url('${CARTAS_RUTA}${CARTAS_NOMBRE}${numeros[contadorNumero]}${CARTAS_EXT}')`;
+                        carta.addEventListener("click", voltearCartaListener);
                         contadorNumero++;
                     } else {
-                        fila.remove(imagen);
+                        fila.remove(carta);
                     }
 
                 }
@@ -55,64 +52,10 @@ function main() {
 
             contCartas.appendChild(fila);
         }
-
-
-        /*
-        // generar filas sin cartas repetidas
-        // **********************************
-        for (let i = 0; i < CARTAS_NUM_FILAS; i++) {
-            let fila = crearDivImagenes(CARTAS_POR_FILA);
-            fila.classList.add("filaCarta");
-            let imagenes = fila.childNodes;
-
-            if (imagenes.length > 0 && numeros.length >= imagenes.length) {
-                let auxiliar = [];
-                for (imagen of imagenes) {
-
-                    let posicion = 0;
-                    let numero = -1;
-                    let salida = true;
-                    do{
-                        numero = numeros[posicion];   // bucle para seleccionar el numero de la carta
-                        
-                        if(numero){
-                            salida = !auxiliar.includes(numero);
-                            if(salida){
-                                auxiliar.push(numero);
-                                numeros.splice(posicion, 1);
-                            } else {
-                                numero = numeros[posicion];
-                            }
-
-                        }
-                        posicion++;
-
-                    }while(numero && !salida && numeros.length > 0);
-
-                    console.log(numeros + "; auxiliar: " + auxiliar);
-
-                    if(numero && numero != -1){
-                        imagen.classList.add("carta");
-                        //imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + numero + CARTAS_EXT;
-                        imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + 0 + CARTAS_EXT;  // ocultar las cartas
-                        imagen.setAttribute("data-index", numero);
-                        imagen.addEventListener("click", voltearCartaListener);
-                    } else {
-                        fila.remove(imagen);
-                    }
-
-                }
-
-            }
-
-            contCartas.appendChild(fila);
-        }
-        */
 
     }
 
 }
-
 
 
 
@@ -121,33 +64,45 @@ function voltearCartaListener(e) {
     let carta = e.target;
     let index = carta.dataset?.index;
 
-    carta.src = CARTAS_RUTA + CARTAS_NOMBRE + index + CARTAS_EXT;
+    carta.classList.add("mostrando");
     carta.classList.add(CLASE_CARTA_MOSTRADA);
+
     carta.removeEventListener("click", voltearCartaListener);
 
     let cartasTablero = document.querySelectorAll("#contenedorCartas ." + CLASE_CARTA_MOSTRADA);
 
-    if(cartasTablero && index){
+    if (cartasTablero && index) {
         cartasTablero = [...cartasTablero];
         let acierto = cartasTablero.every(element => element.dataset.index == index);
 
-        cartasTablero.forEach(function (imagen){
-            
-            if(!acierto || (!acierto && cartasTablero.length >= CARTAS_REPETIDAS)){
-                index = 0;
+        cartasTablero.forEach(function (imagen) {
+
+            if (!acierto || (!acierto && cartasTablero.length >= CARTAS_REPETIDAS)) { // si se falla
                 imagen.classList.remove(CLASE_CARTA_MOSTRADA);
-                imagen.addEventListener("click", voltearCartaListener);
+
+                setTimeout(function () {
+                    imagen.classList.add("ocultando");
+                    imagen.classList.remove("mostrando");
+                }, 600);
+
+                setTimeout(function () {
+                    imagen.classList.remove("ocultando");
+                    imagen.addEventListener("click", voltearCartaListener);
+                }, 900)
+
             }
 
-            if(acierto && cartasTablero.length >= CARTAS_REPETIDAS){
-                index = CARTAS_INVISIBLE;
+            if (acierto && cartasTablero.length >= CARTAS_REPETIDAS) { // si se hace la pareja
                 imagen.classList.remove(CLASE_CARTA_MOSTRADA);
+
+                setTimeout(function () {
+                    imagen.classList.remove("mostrando");
+                    imagen.style.background = "none";
+                }, 600)
+
             }
-        
-            setTimeout(function(){
-                imagen.src = CARTAS_RUTA + CARTAS_NOMBRE + index + CARTAS_EXT;
-            }, 700);
-         });
+
+        });
 
     }
 
@@ -156,14 +111,14 @@ function voltearCartaListener(e) {
 
 
 
-function crearDivImagenes(numImagenes = 0) {
+function crearFilasDiv(numDiv = 0) {
     let contenedor = document.createElement("div");
 
-    if (typeof numImagenes == "number" && numImagenes >= 0) {
+    if (typeof numDiv == "number" && numDiv >= 0) {
 
-        for (let i = 0; i < numImagenes; i++) {
-            let imagen = document.createElement("img");
-            contenedor.appendChild(imagen);
+        for (let i = 0; i < numDiv; i++) {
+            let divElement = document.createElement("div");
+            contenedor.appendChild(divElement);
         }
 
     }
